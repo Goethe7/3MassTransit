9b059460932e35df99b5386b8b65706ac469d4c3
nbrajkovic <novakbee@gmail.com>
2017-04-12 10:38:41 +0100

    tidy-up

--- OrderApproval/Program.cs
+++ OrderApproval/Program.cs
@@ -8,9 +8,9 @@
 
     using MassTransit;
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
 
